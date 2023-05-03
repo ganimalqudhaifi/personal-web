65bc3b50ddb8a6b428b781ed2392851b59bb16eb
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-05-04 05:12:21 +0700

    feat(home_page): add navigation and link to social media

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Index() {
 	return (
@@ -9,12 +10,23 @@ export default function Index() {
 			</Head>
 
 			<div className="w-full min-h-full bg-zinc-900 bg-[url('/svg/diagonal-lines.svg')]">
-				<article className="max-w-6xl px-6 py-24 mx-auto space-y-12 text-gray-50">
-					<div className="w-full mx-auto space-y-4 text-center">
-						<p className="text-xs font-semibold tracking-wider uppercase">#Hello #World</p>
-						<h1 className="text-4xl font-bold leading-tight md:text-5xl">Welcome!!!</h1>
-					</div>
-					<div className="pt-12 border-t border-gray-700">
+				<div className="max-w-6xl px-6 mx-auto space-y-12 text-gray-50">
+
+					<nav className="grid grid-cols-2 py-4 border-b-[0.5px] border-zinc-800">
+						<div className="flex items-center space-x-3">
+							<div className="flex justify-center items-center w-14 h-14 bg-zinc-800 bg-opacity-50 border-[1px] border-zinc-700 p-2 rounded-full">
+								<i className='bx bx-envelope text-xl text-zinc-400'></i>
+							</div>
+							<Link href={'mailto:ganimalqudhaifi@gmail.com'} className="text-sm text-zinc-400 hover:text-zinc-200">Get in Touch</Link>
+						</div>
+						<ul className="flex gap-4 justify-end items-center">
+							<li><Link href={'https://www.linkedin.com/in/andi-muhammad-ganim-alqudhaifi-9669b0214/'} terget="_blank" className="text-sm text-zinc-400 hover:text-zinc-200">Linkedin</Link></li>
+							<li><Link href={'https://github.com/ganimalqudhaifi'} terget="_blank" className="text-sm text-zinc-400 hover:text-zinc-200">Github</Link></li>
+							<li><Link href={'https://www.instagram.com/ganimalqudhaifi/'} terget="_blank" className="text-sm text-zinc-400 hover:text-zinc-200">Instagram</Link></li>
+						</ul>
+					</nav>
+
+					<div className="pt-12">
 						<div className="m-6 space-y-5">
 							<h4 className="text-4xl font-semibold text-center">About Me</h4>
 							<div className="w-full flex items-center justify-center">
@@ -51,7 +63,7 @@ export default function Index() {
 							</a>
 						</div>
 					</div>
-				</article>
+				</div>
 			</div>
 		</>
 	)
