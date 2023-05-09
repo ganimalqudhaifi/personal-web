0ab7f635318b38c8b242341fa18803d6421c4f9a
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-05-09 18:59:57 +0700

    style(about_page): responsive text transform

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -6,8 +6,8 @@ export default function Index() {
     <>
       <WebLayout>
         <div>
-          <div className="m-6 space-y-5">
-            <h4 className="text-4xl font-semibold text-center">
+          <div className="m-6 space-y-7">
+            <h4 className="text-2xl sm:text-3xl md:text-4xl text-gray-300 font-semibold text-center">
               Andi Muhammad Ganim Alqudhaifi
             </h4>
 
@@ -17,27 +17,28 @@ export default function Index() {
               </div>
             </div>
 
-            <div className="space-y-5">
-              <p className="text-gray-400">
+            <div className="space-y-5 text-gray-400">
+              <p>
                 Hi! Saya adalah seorang mahasiswa semester akhir jurusan
                 Manajemen Ekonomi. Sebagai seorang mahasiswa dengan tuntutan
                 untuk belajar, saya juga mengembangkan keterampilan saya dalam
                 bidang pemrograman.{" "}
               </p>
-              <p className="text-gray-400">
-                Saya telah masuk ke dalam dunia pemrograman sejak awal tahun
-                2021 yang pada saat itu pemrograman merupakan sebuah tantangan
-                yang menarik minat saya. Dalam proses belajar, saya mendapat
-                pelatihan dari beberapa sumber seperti Website, Youtube, dan
-                Online Course. Saya juga ikut dalam sebuah komunitas dan
-                menghadiri event event yang relevant terkait dengan Pemrograman.
+              <p>
+                Saya masuk ke dalam dunia pemrograman sejak awal tahun 2021 yang
+                pada saat itu pemrograman merupakan sebuah tantangan yang
+                menarik minat saya terutama ketika menghadapi problem solve.
+                Dalam proses belajar, saya mendapat pelatihan dari beberapa
+                sumber seperti Website, Youtube, dan Online Course. Saya juga
+                ikut dalam sebuah komunitas dan menghadiri event-event yang
+                relevan terkait dengan Pemrograman.
               </p>
-              <p className="text-gray-400">
+              <p>
                 Dalam bidang pemrograman, saya fokus pada pengembangan web,
                 lebih tepatnya sebagai front-end developer. Saya dapat
                 mengerjakan pembuatan website yang bersifat statis maupun yang
                 bersifat dinamis. Dalam pengerjaan web, bahasa yang paling
-                sering saya gunakan adalah bahasa pemrograman javascript. Ada
+                sering saya gunakan adalah bahasa pemrograman Javascript. Ada
                 pula beberapa framework pendukung yang biasa saya gunakan
                 seperti TailwindCSS dan React(CRA, GatsbyJS, NextJS).
               </p>
