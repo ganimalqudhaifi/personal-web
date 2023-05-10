89fb49bcdd2e9b4ead793d38c1e70762251000a4
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-05-10 19:14:10 +0700

    style(WebLayout): responsive text transform and icons

--- components/organisms/WebLayout/index.jsx
+++ components/organisms/WebLayout/index.jsx
@@ -9,9 +9,9 @@ export default function Index({ children }) {
       </Head>
 
       <div className="w-full min-h-full bg-zinc-900 bg-[url('/svg/diagonal-lines.svg')]">
-        <div className="flex flex-col min-h-screen max-w-6xl px-6 mx-auto space-y-12 text-gray-50">
+        <div className="flex flex-col min-h-screen max-w-6xl px-3 sm:px-5 md:px-6 mx-auto space-y-12 text-gray-50">
           <nav className="grid grid-cols-2 py-4 border-b-[0.5px] border-zinc-800">
-            <div className="flex items-center space-x-3 group/mail hover:cursor-pointer">
+            <div className="flex items-center space-x-2 md:space-x-3 group/mail hover:cursor-pointer">
               <div className="flex justify-center items-center w-12 md:w-14 h-12 md:h-14 bg-zinc-800 bg-opacity-50 border-[1px] border-zinc-700 p-2 rounded-full">
                 <i className="bx bx-envelope text-xl text-zinc-400 group-hover/mail:text-zinc-300"></i>
               </div>
@@ -22,7 +22,7 @@ export default function Index({ children }) {
                 Get in Touch
               </Link>
             </div>
-            <ul className="flex gap-4 justify-end items-center">
+            <ul className="flex gap-3 md:gap-4 justify-end items-center">
               <li>
                 <Link
                   href={"/"}
@@ -59,7 +59,7 @@ export default function Index({ children }) {
               © 2023 All Rights Reserved.
             </p>
 
-            <ul className="flex gap-4 justify-end items-center">
+            <ul className="flex gap-3 md:gap-4 justify-end items-center">
               <li>
                 <Link
                   href={
@@ -68,7 +68,8 @@ export default function Index({ children }) {
                   terget="_blank"
                   className="text-sm text-zinc-400 hover:text-zinc-200"
                 >
-                  Linkedin
+                  <i className="text-xl md:hidden bx bxl-linkedin-square"></i>
+                  <span className="hidden md:block">Linkedin</span>
                 </Link>
               </li>
               <li>
@@ -77,7 +78,8 @@ export default function Index({ children }) {
                   terget="_blank"
                   className="text-sm text-zinc-400 hover:text-zinc-200"
                 >
-                  Github
+                  <i className="text-xl md:hidden bx bxl-github"></i>
+                  <span className="hidden md:block">Github</span>
                 </Link>
               </li>
               <li>
@@ -86,7 +88,8 @@ export default function Index({ children }) {
                   terget="_blank"
                   className="text-sm text-zinc-400 hover:text-zinc-200"
                 >
-                  Instagram
+                  <i className="text-xl md:hidden bx bxl-instagram-alt"></i>
+                  <span className="hidden md:block">Instagram</span>
                 </Link>
               </li>
             </ul>
