6c51a3f45682241d873a2f4addbec7ea831734f4
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-10-16 22:37:09 +0700

    style: change PrimaryLayout responsive flex direction

--- components/layouts/PrimaryLayout.jsx
+++ components/layouts/PrimaryLayout.jsx
@@ -8,7 +8,7 @@ export default function PrimaryLayout({ children, activeIndex }) {
 
   return (
     <div className="w-full min-h-screen bg-zinc-900">
-      <div className={`group ${isOpen && 'is-open'} flex flex-col justify-center items-center lg:flex-row lg:pt-10 lg:gap-5`}>
+      <div className={`group ${isOpen && 'is-open'} flex flex-col justify-center items-center lg:flex-row lg:items-start lg:pt-10 lg:gap-5`}>
         {/* TODO: set beahavior to stick */}
         <header className={`sticky top-0 z-10 w-full p-4 bg-zinc-900/95 border-b border-zinc-800 backdrop-blur duration-500 md:p-6 lg:w-auto lg:px-0 lg:py-8 lg:border-0 group-[.is-open]:fixed group-[.is-open]:inset-0 group-[.is-open]:lg:sticky`}>
           <Sidebar activeIndex={activeIndex}/>
