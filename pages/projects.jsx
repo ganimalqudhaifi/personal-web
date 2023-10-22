16a015273744c2ea54cbdc6d7854a1c9ea1ec2db
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-10-22 09:07:07 +0700

    fix: link href in projects

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -19,7 +19,7 @@ export default function Projects() {
         <div className="bg-zinc-600/5 w-full rounded-xl overflow-hidden duration-300 hover:scale-[1.02]">
           <Image alt="financial records project" src="/project-financial-records.png" width={400} height={400} className="w-full"/>
           <div className="p-4 border-2 border-t-0 border-zinc-800/50">
-            <Link href="financial-records.ganimalqudhaifi.my.id" target="_blank" className="inline-block my-3 text-zinc-300 hover:text-zinc-100 duration-300">Financial Records</Link>
+            <Link href="https://financial-records.ganimalqudhaifi.my.id" target="_blank" className="inline-block my-3 text-zinc-300 hover:text-zinc-100 duration-300">Financial Records</Link>
             <p className="text-zinc-400 text-sm">A web application that can track financial transactions in real-time</p>
             <div className="flex space-x-2.5 mt-4 mb-2">
               <SiNextdotjs size={25}/>
