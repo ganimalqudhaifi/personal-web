4962936c97df284e10984dcc63fbc963326b4761
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-10-22 08:46:46 +0700

    chore: revision about me section

--- pages/about.jsx
+++ pages/about.jsx
@@ -16,13 +16,13 @@ export default function About() {
         <p className="text-zinc-300/90">
           Di pertengahan 2020 saya mengikuti seleksi penerimaan sekolah kedinasan. Saya berhasil lulus hingga tahap akhir, yaitu wawancara. Saat itu, salah seorang kandidat yang sedang menunggu giliran untuk di panggil seketika bertanya kepadak saya <q>apa kemampuan yang akan kamu tampilkan?</q>, kemudian saya menjawab dengan percaya diri bahwa saya memiliki kemampuan di bidang IT. Setelah itu dia menanyakan lagi <q>apakah kamu bisa melakukan coding?</q>. Pada waktu itu saya benar-benar tidak mengetahui apa itu coding dan saya merasa malu bahwa saya dengan percaya diri telah mengatakan bahwa saya berkemampuan di bidang IT.
           <br/><br/>
-          Singkat cerita saya gagal pada tahap wawancara tersebut. Beberapa waktu saya memutuskan untuk mendaftar kuliah di salah satu universitas yang cukup terkenal. Mungkin ini terdengar sombong tapi saya memiliki keahlian dalam bidang matematika sejak kecil, sehingga saya memilih untuk mengambil jurusan akuntansi. Pada awal semester, semuanya berjalan dengan lancar, hingga pada akhirnya ketika memasuki semester baru, saya menghadapi masalah finansial dan tidak dapat melanjutkan perkuliahan tersebut.
+          Singkat cerita saya gagal pada tahap wawancara tersebut. Beberapa waktu saya memutuskan untuk mendaftar kuliah di salah satu universitas yang cukup terkenal. Pada awal semester, semuanya berjalan dengan lancar, hingga pada akhirnya ketika memasuki semester baru, saya menghadapi masalah finansial dan tidak dapat melanjutkan perkuliahan tersebut.
           <br/><br/>
-          saya kemudian menganggur selama 3 bulan penuh dan tiba-tiba saya teringat dengan pertanyaan salah satu kandidat saat itu. Di situlah pertama kalinya saya mencoba untuk mempelajari apa itu coding.
+          Saya kemudian menganggur selama 3 bulan penuh dan tiba-tiba saya teringat dengan pertanyaan salah satu kandidat saat itu. Di situlah pertama kalinya saya mencoba untuk mempelajari apa itu coding.
           <br/><br/>
           Mulai pada saat itu, saya berdedikasi untuk mempelajari pemrograman. Melalui beberapa sumber materi belajar yang tersedia, saya belajar secara mandiri. Saya menonton beberapa video di saluran youtube, membaca dokumentasi ringan di W3School, mendapatkan <q>hello world</q> pertama ku, berurusan dengan debugging dan error, hingga pada akhirnya saya mulai menyadari bahwa saya memiliki passion yang kuat pada bidang ini.
           <br/><br/>
-          Sebagai seorang pemrogram, saya sangat menikmati untuk mempelajari hal-hal baru, menyelesaikan masalah, mencari solusi terbaik, dan berkeinginan untuk dapat berkolaborasi dengan pemrogram lainnya.
+          Hingga pada saat ini, sebagai seorang pemrogram, saya sangat menikmati untuk mempelajari hal-hal baru, menyelesaikan masalah, mencari solusi terbaik, dan berkeinginan untuk dapat berkolaborasi dengan pemrogram lainnya.
         </p>
       </div>
     </PrimaryLayout>
