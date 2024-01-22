ec4f02d4390996128510797c8fc5542e9c2285d1
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2024-01-23 04:31:10 +0700

    chore: edit about pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -12,7 +12,7 @@ export default function About() {
       <Divider />
 
       <div>
-        <p className="text-zinc-300/90">
+        {/* <p className="text-zinc-300/90">
           Di pertengahan 2020 ketika saya sedang mengikuti seleksi penerimaan sekolah kedinasan, tepatnya pada saat tahap akhir, yaitu wawancara. Salah seorang kandidat yang sedang menunggu giliran untuk di panggil seketika bertanya kepada saya <q>apa kemampuan yang akan kamu tampilkan?</q> dan saya menjawab dengan percaya diri bahwa saya memiliki kemampuan di bidang IT. Setelah itu dia menanyakan lagi <q>apakah kamu bisa melakukan coding?</q>. Pada waktu itu saya benar-benar tidak mengetahui apa itu coding dan hanya mengetahui kemampuan sederhana seperti excel dan spreadsheet.
           <br/><br/>
           Singkat cerita saya gagal pada tahap wawancara tersebut. Beberapa waktu saya memutuskan untuk mendaftar kuliah jurusan akuntansi di salah satu Universitas. Pada awal semester, semuanya berjalan dengan lancar, hingga pada akhirnya ketika memasuki semester baru, saya menghadapi masalah finansial dan tidak dapat melanjutkan perkuliahan tersebut.
@@ -22,6 +22,9 @@ export default function About() {
           Mulai pada saat itu, saya berdedikasi untuk mempelajari pemrograman. Melalui beberapa sumber materi belajar yang tersedia, saya belajar secara mandiri. Saya menonton beberapa video di saluran youtube, membaca dokumentasi, berurusan dengan debugging dan error, hingga pada akhirnya saya mulai menyadari bahwa saya memiliki passion yang kuat pada bidang ini.
           <br/><br/>
           Sejauh ini, sebagai seorang pemrogram, saya sangat menikmati untuk mempelajari hal-hal baru, mencoba untuk menyelesaikan masalah yang kompleks, mencari solusi terbaik, dan berkeinginan kuat untuk dapat berkolaborasi dengan pemrogram lainnya.
+        </p> */}
+        <p className="text-zinc-300/90">
+          soon...
         </p>
       </div>
     </Layout>
