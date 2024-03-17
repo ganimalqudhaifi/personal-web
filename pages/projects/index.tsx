f7a3eb98f7ac8fe35a19d53303e780b7d0509a7b
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2024-03-17 09:51:32 +0700

    adjust the Image width and heigth for the initial rendering

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -19,7 +19,7 @@ export default function Projects() {
 
       <div className="grid md:grid-cols-2">
         <Link href="/projects/financial-records" className="bg-zinc-600/5 w-full rounded-xl overflow-hidden duration-300 hover:scale-[1.02]">
-          <Image alt="financial records project" src="/project-financial-records.png" width={400} height={400} className="w-full"/>
+          <Image alt="financial records project" src="/project-financial-records.png" width={352} height={193} className="w-full"/>
           <div className="p-4 border-2 border-t-0 border-zinc-800/50">
             <Link onClick={(e) => e.stopPropagation()} href="https://financial-records.ganimalqudhaifi.my.id" target="_blank" className="inline-block my-3 text-zinc-300 hover:text-[#29fd53] duration-300">Financial Records</Link>
             <p className="text-zinc-400 text-sm">A web application that can track financial transactions in real-time</p>
