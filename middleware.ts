502a19c542743e10979ddda832e8c9a8fced8c56
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2024-06-23 22:47:54 +0700

    add middleware for server-side redirect

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import projectList from "./data/projectList";
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/projects")) {
+    const pathname = request.nextUrl.pathname;
+    const segments = pathname.split("/");
+
+    if (segments.length === 3 && segments[1] === "projects") {
+      console.log({ segments });
+      const projectId = segments[2];
+
+      const currentProject = projectList.find(
+        (project) => project.id === projectId
+      );
+      if (!currentProject) {
+        return NextResponse.redirect(new URL("/projects", request.url));
+      }
+    }
+
+    return NextResponse.next();
+  }
+}
