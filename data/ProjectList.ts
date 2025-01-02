25d12451a056a2a9ee2e65f0998edac4546105f6
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2025-01-02 17:26:49 +0700

    Add Discover Videos project

--- data/ProjectList.ts
+++ data/ProjectList.ts
@@ -25,6 +25,23 @@ const projectList = [
       ],
     },
   },
+  {
+    id: "discover-videos",
+    title: "Discover Videos",
+    link: "/projects/discover-videos",
+    githubLink: "https://github.com/ganimalqudhaifi/discover-videos",
+    demoLink: "https://discover-videos.ganimalqudhaifi.my.id",
+    imagePath: "/project-discover-videos.png",
+    description:
+      "A web application that integrate with third party API and oAuth login",
+    techStack: [IconNextJS, IconFirebase, IconTailwindCSS],
+    sections: {
+      introduction: [
+        "Designed and developed using Next.js, the project prioritized web performance to ensure fast loading times and optimal user experience. The goal of this project was to design and develop a comprehensive financial records web application tailored for budgeting purposes. Leveraging modern technologies and best practices, the application aimed to provide users with a seamless and intuitive experience for managing their financial data effectively.",
+      ],
+      keyFeatures: ["Authentication", "Third-Party API"],
+    },
+  },
 ];
 
 export default projectList;
