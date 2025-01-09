ad33608e8de0a9f53bf743adc934757fae9b2315
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2025-01-09 18:49:00 +0700

    Initialize shadcn setup for ui components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ import tailwindTypography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -11,18 +12,71 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        wiggle: "wiggle 2s ease-in-out infinite",
-      },
-      keyframes: {
-        wiggle: {
-          "0%, 80%": { transform: "rotate(0deg)" },
-          "15%, 45%": { transform: "rotate(8deg)" },
-          "30%, 60%": { transform: "rotate(-6deg)" },
-        },
-      },
-    },
+  	extend: {
+  		animation: {
+  			wiggle: 'wiggle 2s ease-in-out infinite'
+  		},
+  		keyframes: {
+  			wiggle: {
+  				'0%, 80%': {
+  					transform: 'rotate(0deg)'
+  				},
+  				'15%, 45%': {
+  					transform: 'rotate(8deg)'
+  				},
+  				'30%, 60%': {
+  					transform: 'rotate(-6deg)'
+  				}
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		}
+  	}
   },
-  plugins: [tailwindTypography],
+  plugins: [tailwindTypography, require("tailwindcss-animate")],
 };
